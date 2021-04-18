c5a55210e1445b75ea36a61b76ed378944d3bd5c
gibifyofficial <evandrogibicoski@outlook.com>
2021-04-17 21:51:02 -0300

    adding links profiles social medias

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaTwitter, FaInstagram, FaWhatsapp, FaFacebook } from 'react-icons/fa';
 
 import styles  from '../styles/Home.module.scss';
 
@@ -6,26 +6,36 @@ const linkGithubProfile = 'https://github.com/gibifyofficial';
 const linkLinkedinProfile = 'https://www.linkedin.com/in/gibifyofficial/';
 const linkTwitterProfile = 'https://twitter.com/gibifyofficial';
 const linkInstagramProfile = 'https://www.instagram.com/gibifyofficial/';
+const linkWhatsAppNumber = 'https://api.whatsapp.com/send?phone=554898463847';
+const linkFacebookProfile = 'https://www.facebook.com/gibifyofficial/';
 
 export default function Home() {
   return (
-      <main className={styles.container}>
-        <div>
-          <FaGithub color="#000000"/>
-          <a href={linkGithubProfile}>GitHub</a>
-        </div>
-        <div>
-          <FaLinkedin color="#000000"/>
-          <a href={linkLinkedinProfile}>LinkedIn</a>
-        </div>
-        <div>
-          <FaTwitter color="#000000"/>
-            <a href={linkTwitterProfile}>Twitter</a>
-        </div>
-        <div>
-          <FaInstagram color="#000000"/>
-            <a href={linkInstagramProfile}>Instagram</a>
-        </div>
+      <main className={styles.container}>  
+          <a href={linkGithubProfile}>
+            <FaGithub color="#000000"/> GitHub
+          </a>
+
+          <a href={linkLinkedinProfile}>
+            <FaLinkedin color="#000000"/> LinkedIn
+          </a>
+        
+          <a href={linkTwitterProfile}> 
+              <FaTwitter color="#000000"/> Twitter
+          </a>
+        
+          <a href={linkInstagramProfile}>
+            <FaInstagram color="#000000"/> Instagram
+          </a>
+
+          <a href={linkFacebookProfile}>
+            <FaFacebook color="#000000"/> Facebook
+          </a>
+
+          <a href={linkWhatsAppNumber}>
+            <FaWhatsapp color="#000000"/> WhatsApp
+          </a>
+
       </main>
   )
 
