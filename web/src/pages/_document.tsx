034e0535dffabd10068cdbf226e849c59daed511
gibifyofficial <evandrogibicoski@outlook.com>
2021-04-28 20:06:40 -0300

    definition meta

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -5,6 +5,17 @@ export default class document extends Document {
         return (
             <Html>
                 <Head>
+                <meta name="author" content="Evandro Gibicoski"/>
+                <meta name="generator" content="Next.js"/>
+                <meta 
+                name="keywords" 
+                content="gibify, Evandro Gibicoski, Font End Developer, Frontend, Typescript, React, JavaScript, CSS3, HTML5, remote, programador, gibifyofficial"
+                />
+                <meta name="description" content="Evandro Gibicoski, Profile Evandro, gibifyofficial"/>
+                
+
+                <meta name="viewport" content="width=device-width, initial-scale"/>
+
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
                 </Head>
