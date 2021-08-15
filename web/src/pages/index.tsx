4f38255008ab990f3edb5f04a811fb6dea7decbd
gibifyofficial <evandrogibicoski@outlook.com>
2021-08-15 18:31:59 -0300

    feat: building mobile layout

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,32 +1,41 @@
+import Image from 'next/image';
+
 import { FaGithub, FaLinkedin, FaTwitter, FaInstagram, FaWhatsapp, FaFacebook, FaDiscord, FaTelegram } from 'react-icons/fa';
-import { SiTiktok } from 'react-icons/si'
-import { IoIosRocket } from 'react-icons/io';
+// import { SiTiktok } from 'react-icons/si'
+// import { IoIosRocket } from 'react-icons/io';
+
 import styles  from '../styles/Home.module.scss';
 
 const linkGithubProfile = 'https://github.com/gibifyofficial';
 const linkLinkedinProfile = 'https://www.linkedin.com/in/gibifyofficial/';
 const linkTwitterProfile = 'https://twitter.com/gibifyofficial';
-const linkInstagramProfile = 'https://www.instagram.com/gibifyofficial/';
-const linkFacebookProfile = 'https://www.facebook.com/gibifyofficial/';
+// const linkInstagramProfile = 'https://www.instagram.com/gibifyofficial/';
+// const linkFacebookProfile = 'https://www.facebook.com/gibifyofficial/';
 const linkDiscordProfile = 'https://discord.com/channels/525719253224128532/gibifyofficial#2086';
-const linkTikTokProfile = 'https://www.tiktok.com/@gibifyofficial?lang=pt-BR';
+// const linkTikTokProfile = 'https://www.tiktok.com/@gibifyofficial?lang=pt-BR';
 const linkWhatsAppNumber = 'https://api.whatsapp.com/send?phone=554898463847';
 const linkTelegramNumber = 'https://msng.link/o/?EvandroGibicoski=tg';
-const linkRocketseatProfile = 'https://app.rocketseat.com.br/me/gibifyofficial';
+// const linkRocketseatProfile = 'https://app.rocketseat.com.br/me/gibifyofficial';
 
 
 export default function Home() {
   return (
       <main className={styles.container}> 
+      <header>
+          <Image src="/images/image-profile.png" alt="Avatar" width={100} height={100}/>
+        <div>
+          <h1>Evandro Gibicoski</h1>
+          <span>@gibifyofficial</span>
+        </div>
+      </header>
       <section className={styles.content}>
         <div className={styles.textWrapper}>
-         <h1><i>Hey 👋, Welcome</i> < br />This is Evandro Gibicoski</h1> 
+         <h1><i>Hey 👋, Welcome</i></h1> 
           <p>
-            As a Frontend Developer, 
-            I am Passionate about Technologies and Innovations,
-            my job is to facilitate the life of all browser users. 
-            I like when a user feels an amazing experience using great Apps.
-            You can feel free to follow me and contact me through of buttons.
+            As a Frontend Developer
+            my job is to facilitate the life of all browser's users. 
+            I love when the user feels an amazing experience using great Apps.
+            You can feel free to contact me through of buttons, and then we can build great apps together.
           </p>
         </div>
 
@@ -43,21 +52,21 @@ export default function Home() {
               <FaTwitter color="#04D361"/> Twitter
           </a>
         
-          <a href={linkInstagramProfile} target="_blank">
+          {/* <a href={linkInstagramProfile} target="_blank">
             <FaInstagram color="#04D361"/> Instagram
           </a>
 
           <a href={linkFacebookProfile} target="_blank">
             <FaFacebook color="#04D361"/> Facebook
-          </a>
+          </a> */}
 
           <a href={linkDiscordProfile} target="_blank">
             <FaDiscord color="#04D361"/> Discord
           </a>
 
-          <a href={linkTikTokProfile} target="_blank">
+          {/* <a href={linkTikTokProfile} target="_blank">
             <SiTiktok color="#04D361"/> TikTok
-          </a>
+          </a> */}
 
           <a href={linkWhatsAppNumber} target="_blank">
             <FaWhatsapp color="#04D361"/> WhatsApp
@@ -67,13 +76,13 @@ export default function Home() {
             <FaTelegram color="#04D361"/> Telegram
           </a>
 
-          <a href={linkRocketseatProfile} target="_blank">
+          {/* <a href={linkRocketseatProfile} target="_blank">
             <IoIosRocket color="#04D361"/> Rocketseat
-          </a>
+          </a> */}
         </div>
         </section>
 
-        <div>
+        <div className={styles.footer}>
           <p>All Rights Reserved by <strong>@gibifyofficial</strong></p>
         </div>
 
